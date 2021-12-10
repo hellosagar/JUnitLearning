c03b71c6d66ec66092e5295016c85f667eacf27f
sagarkhurana <sagarkhurana00786@gmail.com>
2021-12-10 12:53:42 +0530

    Unit Testing basics finished for JUnit 🥳

---
+++ src/test/java/CustomSuite.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({StringHelperParameterizedTest.class, StringHelperTest.class})
+public class CustomSuite {
+}
--- src/test/java/ExceptionsTest.java
+++ src/test/java/ExceptionsTest.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ExceptionsTest {
 
     @Test(expected = NullPointerException.class)
-    public void test_NullArray(){
+    public void test_NullArray() {
         int[] numbers = null;
         Arrays.sort(numbers);
     }
